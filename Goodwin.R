5eafb832e4ec2bd485856bc7949c6fd87a476c19
Bharath Air <bharath.ananthsub@gmail.com>
2018-02-01 21:35:09 +0100

    Shiny app with classic Goodwin oscillator simulation.

---
+++ Goodwin.R
@@ -0,0 +1,74 @@
+library("deSolve")
+library(tidyr)
+library(dplyr)
+library(magrittr)
+library(ggplot2)
+library(gridExtra)
+
+goodwin <- function(t, y, p) {
+  with(as.list(c(y, p)), {
+    dX <- p1*K^h/(K^h + Z^h) - d1*X
+    dY <- p2*X - d2*Y
+    dZ <- p3*Y - d3*Z
+    list(c(X=dX, Y=dY, Y= dZ))
+  })
+}
+
+## prepare data structures to create UI programmatically
+y0    <- c(X=1, Y=0, Z=0)
+parms <- c(p1=1, p2=1, p3=1, d1=0.25, d2=0.25, d3=0.25,K=2, h=4)
+aspect <- 0.8
+
+makelist <- function(i, obj, min=NA, max=NA, step=NA, width=NULL) {
+  list(inputId=names(obj[i]), label=names(obj[i]),
+       value=unname(obj[i]), min=min, max=max, step=step,
+       width=width)
+}
+
+## two lists of lists
+L_parms <- lapply(1:length(parms), makelist, obj=parms, min=0, max=10, step=0.2, width=75)
+L_y0 <- lapply(1:length(y0), makelist, obj=y0, min=0, max=10, step=0.2, width=75)
+
+server <- function(input, output, session) {
+  output$goodwin <- renderPlot({
+    L_input <- reactiveValuesToList(input) # to enable width
+    y0    <- with(L_input, c(X=X, Y=Y, Z=Z))
+    parms <- with(L_input, c(p1=p1, p2=p2, p3=p3, d1=d1, d2=d2, d3=d3, K= K, h =h))
+    times <- seq(0, 150, .1)
+    out <- ode(y0, times, goodwin, parms)
+    df1 <- data.frame(out) %>% gather(var, value,-time)
+    par(mfrow=c(2, 1))
+    f1 <- ggplot(df1, aes(x=time, y=value, color=var)) + geom_line(size=1) + theme_bw() + xlab("Time") + 
+      ylab("Species") + theme(aspect.ratio = 1/4, legend.position = "top", legend.title = element_blank()) +
+      ggtitle("time course")
+    
+    df2 <- data.frame(out, stringsAsFactors = FALSE) %>% arrange(time)
+    f2 <- ggplot(df2, aes(x=X,y=Y)) + geom_path() + theme_bw() + xlab("X") + ylab("Y") + theme(aspect.ratio = 1/4) +
+      ggtitle("phase portrait")
+    grid.arrange(f1, f2, ncol=1)
+  }, height = function() {
+    aspect * session$clientData$output_goodwin_width
+  })
+}
+
+ui <- fluidPage(
+  headerPanel("Goodwin oscillator"),
+  sidebarLayout(
+    sidebarPanel(
+      ## generic creation of UI elements
+      h4("Init values"),
+      lapply(L_y0, function(x) do.call("numericInput", x)),   # <--------
+      
+      h4("Parameters"),
+      lapply(L_parms, function(x) do.call("numericInput", x)), # <--------
+      
+      width = 4
+    ),
+    mainPanel(
+      h4("Simulation results"),
+      plotOutput("goodwin")
+    )
+  )
+)
+
+shinyApp(ui = ui, server = server)
